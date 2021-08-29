3162802e7f89ab1d56824a126fd43719ccbfdb2a
Jay Bajpai <52155590+JayBajpai@users.noreply.github.com>
2021-08-29 11:14:32 +0530

    created a stack Successfully

---
+++ StackProject/stack/stack/MyStack.java
@@ -0,0 +1,27 @@
+package stack;
+
+import linkedlist.INode;
+
+public class MyStack {
+	
+	private final MyLinkedList MyLinkedList;
+	public MyStack(){
+		
+		this.MyLinkedList = new MyLinkedList();
+	}
+	public void  push(INode myNode) {
+		
+		MyLinkedList.add(myNode);	
+		
+	}
+
+	public INode peak() {
+		
+		return MyLinkedList.head;
+		
+	}
+	public void printStack() {
+		MyLinkedList.printNodes();
+		
+	}
+}
